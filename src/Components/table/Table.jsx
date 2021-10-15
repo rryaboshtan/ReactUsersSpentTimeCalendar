afd7fdeccdd92e8a8828e70b23fd0933cc6b4910
rryaboshtan <rrvworkroman@gmail.com>
2021-10-15 18:42:56 +0300

    add users names to the first table column

--- src/Components/table/Table.jsx
+++ src/Components/table/Table.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from 'react';
+import users from './data.json';
 
 function Table() {
-    return (
-       <div>
-          <table className = 'content-table'>
-             <thead>
-                <tr>
-                   <th>user</th>
-                   {[...Array(31).keys()].map(dayNumber => (
-                      <th>{dayNumber + 1}</th>    
-                   ))}
-                   <th>Monthly total</th>
-                </tr>
-             </thead>
-          </table>
-       </div>
-    );
+
+   return (
+      <div>
+         <table className='content-table'>
+            <thead>
+               <tr>
+                  <th>user</th>
+                  {[...Array(31).keys()].map(dayNumber => (
+                     <th>{dayNumber + 1}</th>
+                  ))}
+                  <th>Monthly total</th>
+               </tr>
+               </thead>
+               <tbody>
+                   {users.map(user => {
+                       return (
+                           <tr>
+                               <td>{user.Fullname}</td>
+                           </tr>
+                       )
+                   })
+                   }
+                
+
+               </tbody>
+         </table>
+      </div>
+   );
 }
 
-export default Table
+export default Table;
