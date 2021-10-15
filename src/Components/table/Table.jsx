78be994720c8653a227efac7945b9570c5ed80f8
rryaboshtan <rrvworkroman@gmail.com>
2021-10-15 19:29:54 +0300

    add correct regexp

--- src/Components/table/Table.jsx
+++ src/Components/table/Table.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 import users from './data.json';
 
 function Table() {
+   function mapDaysToTd(days) {
+      const arr = [...Array(31).keys()].map((item, index) => `<td key=${index}>{{minutes}}</td>`);
+
+      console.log(days);
+      let currentIndex = null;
+      let str = '';
+      for (let day of days) {
+         console.log(day);
+
+         if (day) {
+            currentIndex = Number(day.Date.split('-')[2]);
+            //   console.log(currentIndex);
+             str = arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, day.Date);
+             arr[currentIndex - 1] = str;
+         } else {
+            arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, '0');
+         }
+      }
+
+      return arr;
+   }
 
    return (
       <div>
@@ -9,24 +30,23 @@ function Table() {
             <thead>
                <tr>
                   <th>user</th>
-                  {[...Array(31).keys()].map(dayNumber => (
-                     <th>{dayNumber + 1}</th>
+                  {[...Array(31).keys()].map((dayNumber, index) => (
+                     <th key={index}>{dayNumber + 1}</th>
                   ))}
                   <th>Monthly total</th>
                </tr>
-               </thead>
-               <tbody>
-                   {users.map(user => {
-                       return (
-                           <tr>
-                               <td>{user.Fullname}</td>
-                           </tr>
-                       )
-                   })
-                   }
-                
-
-               </tbody>
+            </thead>
+            <tbody>
+               {users.map(user => {
+                  return (
+                     <tr>
+                        <td>{user.Fullname}</td>
+                        
+                        {mapDaysToTd(user.Days)}
+                     </tr>
+                  );
+               })}
+            </tbody>
          </table>
       </div>
    );
