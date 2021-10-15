aafeb74afa42b3a2f43d80757510b0c757145bad
rryaboshtan <rrvworkroman@gmail.com>
2021-10-15 19:41:24 +0300

    show user Days in the table

--- src/Components/table/Table.jsx
+++ src/Components/table/Table.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
 import users from './data.json';
 
+function mapDaysToTd(days) {
+   // const arr = [...Array(31).keys()].map((item, index) => `<td key=${index}>{{minutes}}</td>`);
+
+   // console.log(days);
+   // let currentIndex = null;
+   // let str = '';
+   // // for (let day of days) {
+   //     console.log(day);
+
+   //     if (day) {
+   //         currentIndex = Number(day.Date.split('-')[2]);
+   //         //   console.log(currentIndex);
+   //         str = arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, day.Date);
+   //         arr[currentIndex - 1] = str;
+   //     } else {
+   //         arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, '0');
+   //     }
+   // }
+   // for (let day of days) {
+   //     console.log(day);
+
+   //     if (day) {
+   //         currentIndex = Number(day.Date.split('-')[2]);
+   //         //   console.log(currentIndex);
+   //         str = arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, day.Date);
+   //         arr[currentIndex - 1] = str;
+   //     } else {
+   //         arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, '0');
+   //     }
+   // }
+
+   return days.map((day, index) => <td key={index}>{day.Date}</td>);
+}
+//   return arr.join(' ');
+
 function Table() {
-   function mapDaysToTd(days) {
-      const arr = [...Array(31).keys()].map((item, index) => `<td key=${index}>{{minutes}}</td>`);
-
-      console.log(days);
-      let currentIndex = null;
-      let str = '';
-      for (let day of days) {
-         console.log(day);
-
-         if (day) {
-            currentIndex = Number(day.Date.split('-')[2]);
-            //   console.log(currentIndex);
-             str = arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, day.Date);
-             arr[currentIndex - 1] = str;
-         } else {
-            arr[currentIndex - 1].replace(/\{\{minutes\}\}/gi, '0');
-         }
-      }
-
-      return arr;
-   }
-
    return (
       <div>
          <table className='content-table'>
@@ -41,7 +54,7 @@ function Table() {
                   return (
                      <tr>
                         <td>{user.Fullname}</td>
-                        
+
                         {mapDaysToTd(user.Days)}
                      </tr>
                   );
