591431eb7686be06fb25e1baf432554b11c8f826
rryaboshtan <rrvworkroman@gmail.com>
2021-10-15 20:35:27 +0300

    add true rendering

--- src/Components/table/Table.jsx
+++ src/Components/table/Table.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import users from './data.json';
-import spentMinutes from '../../helpers/helper';
+// import spentMinutes from '../../helpers/helper';
 
 function mapDaysToTd(days) {
-   
-   return days.map((day, index) => <td key={index}>{spentMinutes(day.Start, day.End)}</td>);
+   //    let prefix = '';
+   let dayNumber = null;
+   let lastDayNumber = 1;
+
+   //   return days.map((day, index) => <td key={index}>{day.Date}</td>);
+   //    return days.map((day, index) => <td key={index}>{spentMinutes(day.Start, day.End)}</td>);
+   return days.map((day, index) => {
+      dayNumber = Number(day.Date.split('-')[2]);
+
+      if (dayNumber - lastDayNumber === 2) {
+         lastDayNumber = dayNumber;
+
+         return (
+            <>
+               <td>0</td>
+               <td key={index}> {day.Date}</td>
+            </>
+         );
+      } else if (dayNumber - lastDayNumber === 3) {
+         lastDayNumber = dayNumber;
+
+         return (
+            <>
+               <td>0</td>
+               <td>0</td>
+               <td key={index}> {day.Date}</td>
+            </>
+         );
+      } else {
+         lastDayNumber = dayNumber;
+         return <td key={index}> {day.Date}</td>;
+      }
+   });
 }
 
 function Table() {
